b705d0b3c4ce0b9475ab102d83e6af7ffc864b8d
Joaquín Escayo <j.escayo@csic.es>
2016-10-19 10:14:38 +0200

    v 2.1 - Optional download of AUX_CAL data if CAL_DIR is set

--- README.md
+++ README.md
@@ -6,10 +6,21 @@ It runs in any modern Linux distribution and uses bash and other command line to
 Download folder must be configured before executing it, edit sentinel1_orbit_downloader.sh and modify DOWN_DIR variable.
 It's intended for use within cron daemon, so no output by default to avoid spam on the email.
 
+Installation:
+1. Copy the file "sentinel1_orbit_downloader.sh" to anywhere in your system (recommended /usr/local/bin).
+2. Give execution permissions to the file: chmod +x /path/to/sentinel1_orbit_downloader.sh (change /path/to/ with the path in your system)
+3. Edit sentilel1_orbit_downloader.sh and modify the download directories (if CAL_DIR is not set no AUX_CAL data will be downloaded).
+4. If you are running the program for the first time is recommended to increase the numer of pages to check to download the complete archive (like 100 pages). Modify the varaible PAGES.
+5. Run the program by executing it in bash.
+6. (optional) Add a line in crontab to run the program automatically, for example run "crontab -e" and add the following line:
+15 6 3,17 * * /path/to/sentinel1_orbit_downloader.sh
+Will run the program each day 3th and 17th of the month at 6:15 AM.
+
 Note: I found out that some orbits from March 2015 are missing in Sentinels-1 QC, so I uploaded and can be downloaded from my GitHub repo, https://github.com/krasny2k5/sentinel1_orbit_downloader
 
 Version history:
 v 1.0 - Initial release
 v 2.0 - Included the download of Antenna calibration files (AUX_CAL)
+v 2.1 - Minor revision, now download of AUX_CAL data is optional, directory must be set to download. (19/10/2016)
 
-Your comments and suggestions are welcome
+Your comments and suggestions are welcome, please let me know if you find out this script useful.
--- sentinel1_orbit_downloader.sh
+++ sentinel1_orbit_downloader.sh
@@ -9,6 +9,7 @@
 # Version History:
 # v 1.0 - Initial release
 # v 2.0 - Now it also download EAP Phase calibration files. Bugfixes. (18/10/2016)
+# v 2.1 - Minor revision, now download of AUX_CAL data is optional, directory must be set to download. (19/10/2016)
 # TO-DO:
 # 1. Detection of corrupted files (incompleted downloads)
 
@@ -17,10 +18,10 @@
 ###############################
 # Download directory:
 # Uncomment this line and set the correct directory
-#DOWN_DIR="/home/subsidence/sar-db2/sentinel_precise_orbits" # directory for store precise orbits files
-#CAL_DIR="/home/subsidence/sar-db2/sentinel_aux_cal" # directory for store AUX_CAL files
+DOWN_DIR="" # directory for store precise orbits files
+CAL_DIR="" # directory for store AUX_CAL files
 # number of pages to check
-PAGES=10 # Pages for precise orbits
+PAGES=20 # Pages for precise orbits
 CAL_PAGES=4 # Calibration pages
 
 ###############################
@@ -42,11 +43,11 @@ local_files=$(mktemp /tmp/s1no.XXXXX)
 ###############################
 
 # check if download directory is set
-if [ -z ${DOWN_DIR+x} ]; then
+if [ -z $DOWN_DIR ]; then
     echo "#######################################################"
     echo "You must set download directory before use this program"
     echo "Edit sentinel1_orbit_downloader.sh and set the value of"
-    echo "DOWN_DIR and CAL_DIR variables"
+    echo "DOWN_DIR and (OPTIONAL) CAL_DIR variables"
     echo "#######################################################"
     # cleanup
     rm $list
@@ -63,7 +64,7 @@ if [ ! -d "$DOWN_DIR" ]; then
 fi
 
 # check if CAL_DIR exists
-if [ ! -d "$CAL_DIR" ]; then
+if [ ! -d "$CAL_DIR" ] && [ ! -z "$CAL_DIR" ]; then
   mkdir $CAL_DIR
 fi
 
@@ -112,44 +113,46 @@ cat /dev/null > $remote_files
 cat /dev/null > $dw_list
 cat /dev/null > $local_files
 
-# Calibration files
+# ------------------ #
+# Calibration files  #
+# ------------------ #
+if [ ! -z $CAL_DIR ]; then
+    for i in $(eval echo "{1..$CAL_PAGES}")
+    do
+	    wget --quiet -O - --no-check-certificate https://qc.sentinel1.eo.esa.int/aux_cal/?page=$i >> $index
+    done
 
-for i in $(eval echo "{1..$CAL_PAGES}")
-do
-	wget --quiet -O - --no-check-certificate https://qc.sentinel1.eo.esa.int/aux_cal/?page=$i >> $index
-done
+    # Generando la lista de ficheros a descargar
+    # Me quedo sólo con el nombre de los ficheros
+    grep -o '<a .*href=.*>' $index | grep 'SAFE' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | sort -u > $remote_files
 
-# Generando la lista de ficheros a descargar
-# Me quedo sólo con el nombre de los ficheros
-grep -o '<a .*href=.*>' $index | grep 'SAFE' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | sort -u > $remote_files
+    # compruebo que se haya generado correctamente la lista de archivos a descargar:
+    if ! [ -s $remote_files ]
+    then
+        echo "ERROR OCURRED, NO REMOTE FILES FOUND"
+        # cleanup
+        rm $list
+        rm $index
+        rm $remote_files
+        rm $dw_list
+        rm $local_files
+        exit
+    fi
 
-# compruebo que se haya generado correctamente la lista de archivos a descargar:
-if ! [ -s $remote_files ]
-then
-    echo "ERROR OCURRED, NO REMOTE FILES FOUND"
-    # cleanup
-    rm $list
-    rm $index
-    rm $remote_files
-    rm $dw_list
-    rm $local_files
-    exit
+    # Elimino de la lista los archivos ya descargados
+
+    if [ "$(ls -A $CAL_DIR)" ]; then
+        ls $CAL_DIR > $local_files
+        awk 'NR==FNR{a[$0]=1;next}!a[$0]' $local_files $remote_files > $dw_list
+    else
+        cp $remote_files $dw_list
+    fi
+
+    #Generando los enlaces
+    sed -i 's/^/https:\/\/qc.sentinel1.eo.esa.int\/aux_poeorb\//' $dw_list
+
+    wget --quiet --no-check-certificate -P $CAL_DIR -i $dw_list
 fi
-
-# Elimino de la lista los archivos ya descargados
-
-if [ "$(ls -A $CAL_DIR)" ]; then
-    ls $CAL_DIR > $local_files
-    awk 'NR==FNR{a[$0]=1;next}!a[$0]' $local_files $remote_files > $dw_list
-else
-    cp $remote_files $dw_list
-fi
-
-#Generando los enlaces
-sed -i 's/^/https:\/\/qc.sentinel1.eo.esa.int\/aux_poeorb\//' $dw_list
-
-wget --quiet --no-check-certificate -P $CAL_DIR -i $dw_list
-
 
 ###############################
 #          CLEANUP            #
